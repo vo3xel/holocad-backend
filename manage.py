c20dbc3a5d0ab963810bac96410fa95072c3445b
Matthias Hebenstreit <matthias.hebenstreit@student.tugraz.at>
2020-10-22 14:08:18 +0200

    * added user.py to model * generated migrations

--- manage.py
+++ manage.py
@@ -5,6 +5,8 @@ from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 
 from app.main import create_app, db
+from app.main.model import user
+
 
 #Set the config-mode to dev, prod, or test
 app = create_app(os.getenv('BOILERPLATE_ENV') or 'dev')
