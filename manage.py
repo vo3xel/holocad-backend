44829c1dc13125dc154dbbbc2bd6963997c41a3b
Matthias Hebenstreit <matthias.hebenstreit@student.tugraz.at>
2020-10-22 13:43:35 +0200

    * added config.py to main directory including dev, test and prod configuration * added entry point manage.py of application

---
+++ manage.py
@@ -0,0 +1,35 @@
+import os
+import unittest
+
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
+
+from app.main import create_app, db
+
+#Set the config-mode to dev, prod, or test
+app = create_app(os.getenv('BOILERPLATE_ENV') or 'dev')
+
+app.app_context().push()
+
+manager = Manager(app)
+
+migrate = Migrate(app, db)
+
+manager.add_command('db', MigrateCommand)
+
+@manager.command
+def run():
+    app.run()
+
+@manager.command
+def test():
+    """Runs the unit tests."""
+    tests = unittest.TestLoader().discover('app/test', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    else:
+        return 1
+
+if __name__ == '__main__':
+    manager.run()
